4ad3697d8c6f4a83c92a783188a2814be4516549
Mukul Pal <mukulpal0333@gmail.com>
2025-02-13 11:27:53 +0530

    Introduction To Javascript

---
+++ Phase - 2 ( JS Surgeons )/01 - Introduction To Javascript/Assignments/index.js
@@ -0,0 +1,17 @@
+// create a function that adds two numbers and returns the total.
+
+function total (one, two) {
+    return one + two;
+}
+
+// write a function that subtracts the number of apples given away from the total apples you started with, then returns the remaining apples.
+
+function remainingApples (totalApples, givenAway) {
+    return totalApples - givenAway;
+}
+
+// create a function that multiplies two numbers (chocolate bars per box and number of boxes) and returns the total number of chocolate bars needed.
+
+function totalChocolateBars (barsPerBox, numberOfBoxes) {
+    return barsPerBox * numberOfBoxes;
+}
---
+++ Phase - 2 ( JS Surgeons )/01 - Introduction To Javascript/Code Files/hello.js
@@ -0,0 +1,15 @@
+function printChai () {   // function definition
+    console.log("Hello chai");
+}
+
+function bringBrush (itne) {
+    console.log(`Hanji, le aaya ${itne} brush`);
+}
+
+function addTwo (num1, num2) {
+    return num1 + num2;
+} 
+
+printChai();   // function call
+bringBrush(4);
+console.log(addTwo(4, 2));
