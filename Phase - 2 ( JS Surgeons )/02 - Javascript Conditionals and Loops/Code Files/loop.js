5252314fddb09d8954910a67f4befe682f6807e8
Mukul Pal <mukulpal0333@gmail.com>
2025-02-15 11:39:33 +0530

    Loops

---
+++ Phase - 2 ( JS Surgeons )/02 - Javascript Conditionals and Loops/Code Files/loop.js
@@ -0,0 +1,10 @@
+let teas = ["masala", "ginger", "oolong", "lemon", "rose"]
+
+// loop - iteration
+// variable leke aao
+// limit btao
+// incre / decre
+
+for(let i = 0; i < teas.length; i++) {
+    console.log(`Fruit at index ${i}: ${teas[i]}`);
+}
---
+++ Phase - 2 ( JS Surgeons )/02 - Javascript Conditionals and Loops/Code Files/sum.js
@@ -0,0 +1,12 @@
+let myArray = [1, 2, 3, 4, 5]
+
+function sumOfArray (arr) {
+    let sum = 0;
+    for(let i = 0; i < arr.length; i++) {
+        sum += arr[i]
+    }
+    return sum;
+}
+
+let ans = sumOfArray(myArray)
+console.log(ans);
