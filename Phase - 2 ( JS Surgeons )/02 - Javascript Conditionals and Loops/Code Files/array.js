4479d106b5da5884ab99c57992e97c238746844c
Mukul Pal <mukulpal0333@gmail.com>
2025-02-15 11:21:04 +0530

    Array

---
+++ Phase - 2 ( JS Surgeons )/02 - Javascript Conditionals and Loops/Code Files/array.js
@@ -0,0 +1,11 @@
+let fruits = ["apple", "cherry", "banana"]
+let intFruits = new Array("kiwi", "dragonfruit", "avocado");
+
+fruits[0] = "blueberry"
+fruits.push("new item")
+fruits.unshift("ek aur new item")
+fruits.pop()
+
+console.log(fruits[0]);
+console.log(fruits.length);
+console.log(fruits);
