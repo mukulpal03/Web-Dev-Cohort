ec168bca618a982cc61c12024b9e7c28fdb5b785
Mukul Pal <mukulpal0333@gmail.com>
2025-02-15 10:43:40 +0530

    Data Types

---
+++ Phase - 2 ( JS Surgeons )/02 - Javascript Conditionals and Loops/Code Files/bootcampProfile.js
@@ -0,0 +1,26 @@
+// String - seq of characters 
+let name = "Rohit";
+
+// Number
+let age = 25;
+
+// Boolean
+let isPaid = true;
+
+// null - nothing / empty value
+let favClass = null;
+
+// undefined - not defined
+let homeTown;
+
+// Arrays
+let skills = ["HTML", "CSS", "JS"]
+
+// Object
+let studentProfile = {
+    name: "Haseeb",
+    age: 22,
+    isPaid: true,
+    skills: ["Html", "css", "js"],
+    favClass: null
+}
