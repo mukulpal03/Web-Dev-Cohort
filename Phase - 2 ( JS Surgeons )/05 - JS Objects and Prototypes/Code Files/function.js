bdd13824b667b8b215df5d88002ca731fd5002af
Mukul Pal <mukulpal0333@gmail.com>
2025-02-22 08:55:05 +0530

    Functions

--- Phase - 2 ( JS Surgeons )/05 - JS Objects and Prototypes/Code Files/function.js
+++ Phase - 2 ( JS Surgeons )/05 - JS Objects and Prototypes/Code Files/function.js
@@ -7,3 +7,44 @@
 const add = (x, y) => x + y;
 
 console.log(add(1, 3));
+
+// interview ques
+
+Function.prototype.describe = function () {
+    console.log(`Function name is ${this.name}`);
+}
+
+function masalaChai () {}
+function gingerChai () {}
+
+function greet (name) {
+    return `Hello ${name}`
+}
+
+greet.describe()
+
+function add (a, b) {   // function declaration
+    return a + b;  
+}
+
+const subtract = function (a, b) {   // function expression
+    return a - b;
+}
+
+function applyOperation (a, b, operation) {   // first class function
+    return operation(a, b)
+}
+
+const result = applyOperation(5, 4, (x, y) => x * y)
+
+function createCounter () {  // Closures
+    let count = 0;
+    return function () {
+        count++
+        return count
+    }
+}
+
+(function () {  
+    console.log("hitesh");
+})()     // IIFE (immediately invoked function expression) - suppose we creating a web application and app start hote hi database connection ka function call ho jaye
