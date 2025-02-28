53929df862a2135984a6d9e0e894bb50b6f2505b
Mukul Pal <mukulpal0333@gmail.com>
2025-02-28 18:08:58 +0530

    Real Time Form Input Display

--- Phase - 2 ( JS Surgeons )/06 - Introduction to DOM/Assignments/Challenge - 3 (Real Time Form Input display)/app.js
+++ Phase - 2 ( JS Surgeons )/06 - Introduction to DOM/Assignments/Challenge - 3 (Real Time Form Input display)/app.js
@@ -1,3 +1,24 @@
-/**
- * Write your challenge solution here
- */
+let nameInput = document.querySelector('#nameInput');
+let jobInput = document.querySelector('#jobInput');
+let ageInput = document.querySelector('#ageInput');
+let bioInput = document.querySelector('#bioInput');
+let nameDisplay = document.querySelector('#nameDisplay');
+let jobDisplay = document.querySelector('#jobDisplay');
+let ageDisplay = document.querySelector('#ageDisplay');
+let bioDisplay = document.querySelector('#bioDisplay');
+
+nameInput.addEventListener('input', () => {
+     nameDisplay.textContent = nameInput.value || "Not Provided"
+})
+
+jobInput.addEventListener('input', () => {
+     jobDisplay.textContent = jobInput.value || "Not Provided"
+})
+
+ageInput.addEventListener('input', () => {
+    ageDisplay.textContent = ageInput.value || "Not Provided"
+})
+
+bioInput.addEventListener('input', () => {
+    bioDisplay.textContent = bioInput.value || "Not Provided"
+})
