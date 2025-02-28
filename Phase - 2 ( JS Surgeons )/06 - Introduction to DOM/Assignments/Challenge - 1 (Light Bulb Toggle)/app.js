fb066f58aa3061147ef3c2eb3d17492bbcc10de7
Mukul Pal <mukulpal0333@gmail.com>
2025-02-28 17:12:35 +0530

    DOM Challenges

---
+++ Phase - 2 ( JS Surgeons )/06 - Introduction to DOM/Assignments/Challenge - 1 (Light Bulb Toggle)/app.js
@@ -0,0 +1,18 @@
+/**
+ * Write your challenge solution here
+ */
+
+const bulb = document.querySelector('#bulb');
+const toggleBtn = document.querySelector('#toggleButton')
+const statusMsg = document.querySelector('#status')
+const darkModeTextToggle = document.querySelectorAll('.dark')   // to change all the texts to white if theme is dark else black
+
+toggleBtn.addEventListener("click", () => {
+    const isOff = bulb.classList.toggle("off");    // here we are toggling the class to bulb and bulb off
+
+    darkModeTextToggle.forEach(element => element.classList.toggle('dark'));   // here we are toggling the class of the texts
+
+    toggleBtn.innerText = isOff ? 'Turn On' : 'Turn Off';
+    statusMsg.innerText = `Status: ${isOff ? 'Off' : 'On'}`;
+    document.body.style.backgroundColor = isOff ? 'black' : 'white';
+})
