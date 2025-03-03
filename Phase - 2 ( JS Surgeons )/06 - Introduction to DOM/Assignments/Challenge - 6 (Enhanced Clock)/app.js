5a71f9ef3eb33bda2620c9c818b0fc5099aaecca
Mukul Pal <mukulpal0333@gmail.com>
2025-03-03 12:42:58 +0530

    Enhanced Clock

--- Phase - 2 ( JS Surgeons )/06 - Introduction to DOM/Assignments/Challenge - 6 (Enhanced Clock)/app.js
+++ Phase - 2 ( JS Surgeons )/06 - Introduction to DOM/Assignments/Challenge - 6 (Enhanced Clock)/app.js
@@ -1,5 +1,9 @@
 const dateDisplay = document.querySelector('.date')
 const digitalClock = document.querySelector('.digital-clock')
+const clock = document.querySelector(".clock");
+const handHour = document.querySelector('.hour')
+const handMinute = document.querySelector('.minute')
+const handSec = document.querySelector('.second')
 
 const date = new Date()
 
@@ -8,6 +12,22 @@ dateDisplay.textContent = date.toDateString()
 setInterval(() => {
     const date = new Date()
     digitalClock.textContent = `${date.toLocaleTimeString()}`
+
+    const seconds = date.getSeconds();
+    const minutes = date.getMinutes();
+    const hours = date.getHours() % 12;
+
+    handHour.style.transform = `translateX(-50%) rotate(${30 * hours}deg)`
+    handMinute.style.transform = `rotate(${6 * minutes}deg)`
+    handSec.style.transform = `rotate(${6 * seconds}deg)`
 }, 1000)
 
-
+for(let i = 1;i<=12;i++){
+    const number = document.createElement("div");
+    number.classList.add('number')
+    number.style.setProperty("--rotation", `${i * 30}deg`);
+    const span = document.createElement("span");
+    span.innerText = i;
+    number.appendChild(span)
+    clock.appendChild(number)
+}
